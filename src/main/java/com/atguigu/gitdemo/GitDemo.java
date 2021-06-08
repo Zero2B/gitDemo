efd160b8d57ff109c676ca641b109218f5ca7ae3
夜云 <275451335@qq.com>
2021-06-09 00:11:52 +0800

    idea，hotfix测试

--- src/main/java/com/atguigu/gitdemo/GitDemo.java
+++ src/main/java/com/atguigu/gitdemo/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
     public static void main(String[] args){
         System.out.println("idea的git测试一");
         System.out.println("idea的git测试二");
+        System.out.println("hotfix的git测试");
     }
 }
