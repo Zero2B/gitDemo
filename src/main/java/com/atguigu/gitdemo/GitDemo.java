2f6aa38f36b929bfaac0ccf4062c86c344c90b68
夜云 <275451335@qq.com>
2021-06-09 00:04:45 +0800

    idea测试

---
+++ src/main/java/com/atguigu/gitdemo/GitDemo.java
@@ -0,0 +1,11 @@
+package com.atguigu.gitdemo;
+
+/**
+ * @author liuxing
+ * @create 2021-06-08 23:52
+ */
+public class GitDemo {
+    public static void main(String[] args){
+        System.out.println("idea的git测试一");
+    }
+}
